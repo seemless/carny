6aa10bc3781baf329939653f88c19d37ba5d3c9c
Matt <matthew.christopher.smith@gmail.com>
2012-07-26 15:01:29 -0400

    added 2 new time formats: .log, appache access logs

--- carny.py
+++ carny.py
@@ -10,12 +10,14 @@ time_patterns = {
                  #win_user.log, error_log, 
                  r"\w+\s+\w+\s+\d{,2}\s+\d{,2}:\d{,2}:\d{,2}\s+\d{,4}":"%a %b %d %H:%M:%S %Y",
                  #_maillog, _messages, _secure
-                 r"\w+\s+\d{,2}\s+\d{,2}:\d{,2}:\d{,2}":"%b %d %H:%M:%S", 
+                 #TODO: add year to above messages
+                 r"\w+\s+\d{,2}\s+\d{,2}:\d{,2}:\d{,2}":"%b %d %H:%M:%S %Y", 
                  # _last.log,
                  #ossec.alert.log,
                  #{01/Oct/2006:17:52:01 -0400} access
+                 r"\d{,2}\/\w+\/\d{,4}:\d{,2}:\d{,2}:\d{,2}\s+\-\d{,4}":"%d/%b/%Y:%H:%M:%S -0400",
                  #{2006-10-1       21:14:31} .log
-                 
+                 r"\d{,4}-\d{,2}-\d{,2}\s+\d{,2}:\d{,2}:\d{,2}":"%Y-%m-%d %H:%M:%S"
                  
     }
 
@@ -24,7 +26,7 @@ def main():
             "9/16/2006 9:13:11 PM",
             "Sun Oct  1 22:38:22 2006",
             "Mon Oct 02 13:27:23 2006",
-            "Oct  1 22:46:50",
+            "Oct  1 22:46:50 2006",
             "01/Oct/2006:17:52:01 -0400",
             "2006-10-1       21:14:31",
             ]
@@ -42,6 +44,7 @@ def guess(date_time_str):
     for pat in time_patterns:
         m = re.compile(pat).match(date_time_str)
         if m is not None:
+            print(date_time_str)
             fmt = time_patterns[pat]
             epoch = get_epoch(date_time_str,fmt)
             return epoch
